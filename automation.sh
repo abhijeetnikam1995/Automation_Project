e2cf05c91eeb6f9e6da2e4eee2c078d266857357
root <abhijeetnikam1995@gmail.com>
2022-05-03 12:43:27 +0530

    “Automation-vo.2”

--- automation.sh
+++ automation.sh
@@ -30,3 +30,19 @@ if [ -f "$file" ];
 then
      aws s3 cp /tmp/${name}-httpd-logs-${timestamp}.tar s3://upgrad-abhijeet/${name}-httpd-logs-${timestamp}.tar
 fi
+
+path="/var/www/html"
+if [ ! -f ${path}/inventory.html ];
+then
+    echo -e 'Log Type\t-\tTime Created\t-\tFile Type\t-\tFile Size' > /var/www/html/inventory.html
+fi
+if [[ -f ${path}/inventory.html ]];
+then
+    size=$(du -h /tmp/${name}-httpd-logs-*.tar | awk '{print $1}')
+	echo -e "httpd-logs\t-\t${timestamp}\t-\ttar\t-\t${size}" >> ${path}/inventory.html
+fi
+if [[ ! -f /etc/cron.d/Automation ]];
+then
+    echo " * * * * * root/Automation_Project/Automation.sh" >> /etc/cron.d/Automation
+fi
+
